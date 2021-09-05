12e6f176c0b8bb74abd08c9234332a0f32729746
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-09-05 03:32:49 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+const passport = require('passport');
+const localStrategy = require('./auth/local');
+const jwtStrategy = require('./auth/jwt');
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(expressValidator());
+app.use(morgan('common'));
+app.use(passport.initialize());
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+require('./routes')(app);
+
+module.exports = app;
+
+require("./config/passport")(passport);
+
+connectDB();
+
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select
+} = require("./helpers/hbs");
+
+// Handlebars
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select
+    },
+    defaultLayout: "main",
+    extname: ".hbs"
+  })
+);
+app.set("view engine", ".hbs");
+
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+
+// Passport middleware
+//app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
